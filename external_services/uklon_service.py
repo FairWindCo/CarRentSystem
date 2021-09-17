2546ffca75142760607adc8a1b4f4acb0981410c
FairWind <sergey.manenok@gmail.com>
2021-09-17 16:50:26 +0300

    try add custom list view in admin

--- external_services/uklon_service.py
+++ external_services/uklon_service.py
@@ -1,14 +1,20 @@
-from datetime import datetime
+import time
+from datetime import datetime, timedelta
 
 import environ
 import requests
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class UklonTaxiService:
     base_url = 'https://partner.uklon.com.ua/'
     api = 'api/v1/'
     partner_api = ''
-    headers = {'user-agent': 'my-app/0.0.1'}
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'}
 
     def __init__(self, user: str, password: str):
         self.user = user
@@ -18,36 +24,102 @@ class UklonTaxiService:
         self.drivers = {}
         self.cars = {}
         self.car_driver_datas = None
+        self.uid = None
+        self.use_silenium = False
 
-    def connect(self):
-        if self.session is not None:
-            self.logout()
+    def wait_page(self, browser, element_id):
+        try:
+            myElem = WebDriverWait(browser, 10).until(
+                expected_conditions.presence_of_element_located((By.ID, element_id)))
+            print("Page is ready!")
+            return True
+        except TimeoutException:
+            print("Loading took too much time!")
+            return False
+
+    def connect(self, silenium=True):
         session = requests.Session()
-        response = session.post(f'{self.base_url}{self.api}login', json={
+
+        if silenium:
+            from selenium import webdriver
+
+            browser = webdriver.Firefox(executable_path='E:\Downloads\geckodriver-v0.30.0-win64\geckodriver.exe',
+                                        firefox_profile='./profile')
+            browser.get('https://partner.uklon.com.ua')
+            self.use_silenium = browser
+            user_field = browser.find_element_by_name('login')
+            if user_field:
+                user_field.send_keys(self.user)
+                pass_field = browser.find_element_by_name('loginPassword')
+                if pass_field:
+                    pass_field.send_keys(self.password)
+                    remembe_field = browser.find_element_by_xpath('//label[@for="rememberMe"]')
+                    remembe_field.click()
+                    time.sleep(3)
+                    sendbtn = browser.find_element_by_name('Login')
+                    if sendbtn:
+                        sendbtn.click()
+                        if not self.wait_page(browser, 'logoutForm'):
+                            return False
+                        browser.get('https://partner.uklon.com.ua/partner/vehicle')
+                        if not self.wait_page(browser, 'logoutForm'):
+                            return False
+                        for cookie in browser.get_cookies():
+                            session.cookies.set(cookie['name'], cookie['value'], domain=cookie['domain'])
+                            # c = {cookie['name']: cookie['value'], domain=cookie['domain']}
+                            # session.cookies.update(c)
+                            # print(c)
+                        selenium_user_agent = browser.execute_script("return navigator.userAgent;")
+                        app_uid = browser.execute_script('n=window.localStorage,r=n.getItem("app_uid");')
+                        self.headers = {"user-agent": selenium_user_agent,
+                                        'app_uid': app_uid,
+                                        'Referer': 'https://partner.uklon.com.ua/'}
+                        self.session = session
+                        return True
+            # session.cookies = cookies
+        self.use_silenium = False
+        json = {
             'login': self.user,
             'password': self.password,
             'persist': True
-        }, headers=self.headers)
+        }
+        url = f'{self.base_url}{self.api}login'
+        response = session.post(url, json=json, headers=self.headers)
         if response.status_code == 200:
             self.session = session
             return True
+        # print(response.text, url, json)
         return False
 
-    def _send_api_request(self, endpoint: str = 'me', method: str = 'POST', api: str = '', json: dict = None,
-                          data: dict = None, debug=False) -> dict:
-        url = f'{self.base_url}{api}{endpoint}'
+    def _send_request(self, url: str = '/', method: str = 'POST',
+                      json: dict = None,
+                      data: dict = None,
+                      params: dict = None,
+                      debug=False, return_json=True) -> (bool, int, dict):
         if debug:
             print(url)
+            print(self.headers)
+            print(self.session.cookies)
+
         if self.session is not None:
-            if method == 'POST':
-                response = self.session.post(url, data=data, json=json,
-                                             headers=self.headers)
-            else:
-                response = self.session.get(url, params=data, json=json,
-                                            headers=self.headers)
-            if response.status_code == 200:
-                return response.json()
-        return {}
+            method = getattr(self.session, method.lower(), None)
+            if method:
+                response = method(url, data=data, json=json, params=params,
+                                  headers=self.headers)
+                if response.status_code == 200:
+                    if return_json:
+                        return True, response.status_code, response.json()
+                    else:
+                        return True, response.status_code, response.text
+                else:
+                    return False, response.status_code, {}
+        return False, 0, {}
+
+    def _send_api_request(self, endpoint: str = 'me', method: str = 'POST', api: str = '', json: dict = None,
+                          data: dict = None, params=None, debug=False) -> dict:
+        url = f'{self.base_url}{api}{endpoint}'
+        _, _, obj = self._send_request(url, method, json, data, params, debug)
+        return obj
 
     def get_uid__balance(self, uid):
         result = {}
@@ -107,8 +179,36 @@ class UklonTaxiService:
             params['vehicleId'] = vehicle_id
 
         res = self._send_api_request(endpoint=f'fleets/{self.uid}/history-rides',
-                                     method='GET', debug=True, api=self.api, data=params)
+                                     method='GET', debug=False, api=self.api, params=params)
         return res
+
+    def get_day_rides(self, day=None, vehicle_id: str = None, limit=None):
+        if day is None:
+            day = datetime.now()
+        start_time = time.mktime(day.date().timetuple())
+        end_time = time.mktime((day + timedelta(days=1)).timetuple())
+        count = 0
+        page = 1
+        response = []
+        next_iteration = True
+        while next_iteration and (limit is None or count < limit):
+            result = self.get_rides(start_time, end_time, vehicle_id=vehicle_id, page=page)
+            if result:
+                if 'collection' in result:
+                    trips = result['collection']
+                    page = page + 1
+                    for trip in trips:
+                        if trip['pickup_time'] <= end_time and trip['pickup_time'] >= start_time:
+                            response.append(trip)
+                        else:
+                            next_iteration = False
+                    if next_iteration:
+                        next_iteration = ('hasNext' in result and result['hasNext'])
+                else:
+                    break
+            else:
+                break
+        return response
 
     def process_car_driver_data(self, car_driver_data):
         self.drivers.clear()
@@ -143,12 +243,10 @@ class UklonTaxiService:
         return res
 
     def logout(self):
-        if self.session is not None:
-            response = self.session.post(f'{self.base_url}account/logout')
-            if response.status_code == 200:
-                self.session = None
-                return True
-        return False
+        res, _, _ = self._send_request(f'{self.base_url}account/logout', return_json=False)
+        if self.use_silenium:
+            self.use_silenium.close()
+        return res
 
 
 if __name__ == '__main__':
@@ -159,6 +257,7 @@ if __name__ == '__main__':
     environ.Env.read_env()
     user_name = env('UKLON_USER')
     user_pass = env('UKLON_PASS')
+    # print(user_name, user_pass)
     uklon = UklonTaxiService(user_name, user_pass)
     print(uklon.connect())
     print((uklon.get_my_info()))
@@ -171,7 +270,7 @@ if __name__ == '__main__':
     print(uklon.cars)
     print(uklon.drivers)
     print(uklon.get_rides())
+    print(uklon.get_day_rides())
     print(uklon.logout())
 
-
-#https://partner.uklon.com.ua/partner/finances/search?page=1&pageSize=20&startDate=1631480400&endDate=1631739600
+# https://partner.uklon.com.ua/partner/finances/search?page=1&pageSize=20&startDate=1631480400&endDate=1631739600
