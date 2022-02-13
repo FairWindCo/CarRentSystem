d4ae98ccfd296bfc6925bb582f5eaa25df59c6fc
FairWind <sergey.manenok@gmail.com>
2022-02-13 20:24:22 +0200

    add support mysql

--- CarRentSystem/settings.py
+++ CarRentSystem/settings.py
@@ -147,6 +147,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': env.str('DB_NAME'),
+        # 'USER': env.str('DB_USER'),
+        # 'PASSWORD': env.str('DB_PASS'),
+        # 'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
+        # 'PORT': '3306',
     }
 }
 
